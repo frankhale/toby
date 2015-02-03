e90928ae9c0c582c8a9945818292994abcd95b5e
Frank Hale <frankhale@gmail.com>
2015-02-03 10:35:55 -0500

    Expand search capability & refine search algorithm
    - Make the search algorithm better
    - Sort the results in alphabetical order
    - Add searching by group title

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 Toby
 ====
 
-A small and simple YouTube video player written for Atom-Shell. Toby takes the
-path of least resistance. It provides a way to specify videos and a way to
-look them up and play them. Nothing more, nothing less. Getter done! Play the
-videos you really like and love life!
+A desktop YouTube video player written for Atom-Shell. Toby takes the path of 
+least resistance. It provides a way to specify videos and a way to look them up 
+and play them. Nothing more, nothing less. Getter done! Play the videos you 
+really like and love life!
 
 ##Screenshots
 
@@ -16,47 +16,58 @@ Search your playlist for a video
 
 <img src="screenshots/toby-video-search.png" alt="Video Selection"/>
 
-List all songs in your playlist
+List all videos in your playlist:
+
+By typing %all% into the search box you will get a listing of all of your
+videos.
 
 <img src="screenshots/toby-video-search-all.png" alt="Video Selection - List All"/>
 
+List videos by group name:
+
+By wrapping your video group with % you can get a listing of all videos in a
+specific group.
+
+<img src="screenshots/toby-video-search-by-group.png" alt="Video Selection - List By Group"/>
+
 Recently played list
 
-<img src="screenshots/toby-recently-played.png" alt="Video Selection"/>
+<img src="screenshots/toby-recently-played.png" alt="Recently Played List"/>
 
 ##Dependencies
 
-Toby is written in JavaScript / HTML and CSS and needs Atom-Shell to run:
+If you want to run the latest code from master feel free. Toby requires
+Atom-Shell to run.
 
-https://github.com/atom/atom-shell
+Atom-Shell: https://github.com/atom/atom-shell
+
+NOTE: I'll write instructions on how to make Toby run in Atom-Shell soon. If 
+you just want to try Toby out then download a release (see below). 
+Unfortunately I only have Windows binaries at the moment.
 
 ##Release(s)
 
 0.7.0 - https://github.com/frankhale/toby/releases/tag/v0.7.0
 
-I've made a Windows binary available which packages Toby and Atom-Shell together and
-will make it super simple to get started.
+I've made a Windows binary available which packages Toby and Atom-Shell 
+together and will make it super simple to get started.
 
-NOTE: I'd gladly accept Linux and/or Mac binaries, please let me know if you can 
-assist!
-
-##Requirements
-
-Atom-Shell
+NOTE: I'd gladly accept Linux and/or Mac binaries, please let me know if you 
+can assist!
 
 ##Usage
 
-The data folder has a JSON file in it which is pretty self explanatory. There is
-an array of genres which have a set of videos that go with them. Basically what
-you do is you just grab the title of the video and the embed URL from YouTube
-and add it manually (for now) to the file. You can do this while the app is
-running and the new entry will show up in your play list.
+The data folder has a JSON file in it which is pretty self explanatory. There 
+is an array of genres which have a set of videos that go with them. Basically 
+what you do is you just grab the title of the video and the embed URL from 
+YouTube and add it manually (for now) to the file. You can do this while the 
+app is running and the new entry will show up in your play list.
 
 The data.json file has the following format:
 
-There is just one big array of video groups, groups contain the title of the groups
-and the videos associated with it. Videos are just an array of files that contain a
-description and a URL.
+There is just one big array of video groups, groups contain the title of the 
+groups and the videos associated with it. Videos are just an array of files 
+that contain a description and a URL.
 
 ```json
 [{  
@@ -82,14 +93,15 @@ F12 - Open dev tools
 
 ##NOTE
 
-As of Atom-Shell 0.19.2 the Http Referrer patch that I wrote has been incorporated and
-videos that are blocked due the referrer not being set will now work. I won't mention
-any names of publishers that do this but there is a major one and it now works!
+As of Atom-Shell 0.19.2 the Http Referrer patch that I wrote has been 
+incorporated and videos that are blocked due the referrer not being set will 
+now work. I won't mention any names of publishers that do this but there is a 
+major one and it now works!
 
 ##Author(s)
 
 Frank Hale &lt;frankhale@gmail.com&gt;  
-1 February 2015
+3 February 2015
 
 ##License
 
--- src/toby.js
+++ src/toby.js
@@ -3,8 +3,15 @@
 //
 // Status: This is a work in progress, this is a rewrite using React.js.
 //
+// NOTE: I have not compiled my JSX because I've hacked the JSXTransformer.js
+//       to not barf on Atom-Shell's webview tag. I'm just not interested ATM
+//       to try to figure out another way to use the webview tag. I really like
+//       the JSX syntax and I think it works well to describe what is going on.
+//       Because of this I cannot compile the JSX because the JSX compiler will
+//       barf when it finds the webview tag.
+//
 // Frank Hale <frankhale@gmail.com>
-// 1 February 2015
+// 3 February 2015
 //
 
 "use strict";
@@ -61,7 +68,7 @@ var TobyReact = (function() {
     },
     addToRecentlyPlayedList: function(v) {
       var found = _.find(this.state.recentlyPlayedData, function(video) {
-        if (video.title === v.title) {
+        if (video.description === v.description) {
           return v;
         }
       });
@@ -69,7 +76,7 @@ var TobyReact = (function() {
       if (found === undefined) {
         var recentlyPlayed = this.state.recentlyPlayedData;
         v.playVideo = function() {
-          this.props.playVideo(v.title, v.url);
+          this.props.playVideo(v.description, v.url);
         }.bind(this);
         recentlyPlayed.push(v);
         this.setState({ 
@@ -79,23 +86,30 @@ var TobyReact = (function() {
     },
     handleClick: function(e) {
       var url = e.target.dataset.url + "?autoplay=1",
-          title = e.target.text;
+          description = e.target.text;
 
       this.addToRecentlyPlayedList({
-        'title': title,
+        'description': description,
         'url': url
       });
           
       this.state.searchResults.css('display', 'none');
-      this.props.playVideo(title, url);
+      this.props.playVideo(description, url);
     },
     render: function() {
       var bindClick = this.handleClick.bind(this);
-    
+
       return (
-        <div>
+        <div style={this.props.style}>
           <div id="searchResults">
-            {this.props.data.map(function(r) {
+            {this.props
+                 .data
+                 .sort(function(a, b) {
+                    if(a.description < b.description) return -1;
+                    if(a.description > b.description) return 1;
+                    return 0;
+                 })
+                 .map(function(r) {
               return <span><a href='#' data-url={r.url} onClick={bindClick}>{r.description}</a><br /></span>
             })}
           </div>
@@ -118,8 +132,15 @@ var TobyReact = (function() {
         <div id="recentlyPlayed" style={this.props.style}>
           <div id="recentlyPlayedHeader">Recently Played</div>
           <div id="recentlyPlayedList">
-            {this.props.data.map(function(r) {
-              return <span><a href='#' data-url={r.url} onClick={r.playVideo}>{r.title}</a><br /></span>
+            {this.props
+                 .data
+                 .sort(function(a, b) {
+                    if(a.description < b.description) return -1;
+                    if(a.description > b.description) return 1;
+                    return 0;
+                 })
+                 .map(function(r) {
+              return <span><a href='#' data-url={r.url} onClick={r.playVideo}>{r.description}</a><br /></span>
             })}
           </div>
         </div>
@@ -150,7 +171,10 @@ var TobyReact = (function() {
         
       this.state.browser.setTitle(this.state.searchPlayListTitle);
       this.loadDataFile(function(data) {
-        this.setState({videoData: data});
+        this.setState({
+          videoData: data,
+          videos: _.flatten(_.pluck(data, "videos"))
+        });
       }.bind(this));
 
       this.setState({
@@ -183,7 +207,7 @@ var TobyReact = (function() {
         }
       });
     },
-    toggleSearchPlayListAndWebview: function(title, url) {      
+    toggleSearchPlayListAndWebview: function(description, url) {      
       if(url !== undefined) {
         this.state.web.attr("src", url);
       }
@@ -199,10 +223,11 @@ var TobyReact = (function() {
         
         this.setState({ 
           searchListStyle: { display: "none" },
+          searchResultsStyle: { display: "none" },
           webviewStyle: { visibility: "visible" }
         });
 
-        if(title === undefined) {
+        if(description === undefined) {
           if(this.state.currentVideoTitle !== undefined && 
             this.state.currentVideoTitle !== "") {
             this.state.browser.setTitle(this.state.currentVideoTitle);
@@ -216,43 +241,54 @@ var TobyReact = (function() {
         });
       }
     },
-    playVideo: function(title, url) {
+    playVideo: function(description, url) {
       if (this.state.web.attr('src') !== null && this.state.webview.isLoading()) {
         this.state.webview.stop();
       }
 
       this.setState({ 
-          currentVideoTitle: title,
+          currentVideoTitle: description,
           searchResultData: []
       });
 
-      this.state.browser.setTitle(title);
-      this.toggleSearchPlayListAndWebview(title, url);
+      this.state.browser.setTitle(description);
+      this.toggleSearchPlayListAndWebview(description, url);
     },
     handleSearch: function(e) {
-      if(e.target.value.length === 0) {
+      var results = [];
+      var searchTerm = e.target.value.toLowerCase();
+      
+      if(searchTerm.length === 0) {
         this.setState({searchResultData: []});
         return;
       }
 
-      var results = [];
-      var all = false;
+      if(searchTerm === "%all%") {
+        console.log(this.state.videos);
+        results = this.state.videos.slice(0);
+      } else if(searchTerm.charAt(0) === "%" && searchTerm.slice(-1) === "%") {
+        var videoGroup = _.find(this.state.videoData, function(g) {
+          return searchTerm === "%" + g.title.toLowerCase() + "%";
+        });
 
-      if(e.target.value === "%all%") {
-        all = true;
-      }
-      
-      _.forEach(this.state.videoData, function(g) {
-        if (g.title !== undefined && g.videos !== undefined) {
-          _.forEach(g.videos, function(v) {
-            if (all || v.description.toLowerCase().startsWith(e.target.value.toLowerCase())) {
-              results.push(v);
-            }
-          });
+        if(videoGroup !== undefined) {
+          results = videoGroup.videos.slice(0);
         }
-      });
+      } else {
+        results = _.filter(this.state.videos, function(v) {
+          return v.description.toLowerCase().startsWith(searchTerm);
+        });
+      }
 
-      this.setState({searchResultData: results});
+      if(results.length > 0) {
+        this.setState({
+          searchResultData: results,
+          searchResultsStyle: { display: "block" }
+        });
+      }
+      else {
+        this.setState({searchResultData: results});
+      }
     },
     handleKeyDown: function(e) {
       switch(e.keyCode) {
@@ -272,9 +308,9 @@ var TobyReact = (function() {
         <div>
          <div id="searchList" style={this.state.searchListStyle}>
             <input type="text" id="searchBox" placeholder="search for videos..." onChange={this.handleSearch}></input>
-            <SearchResultsList data={this.state.searchResultData} playVideo={this.playVideo} />
+            <SearchResultsList data={this.state.searchResultData} playVideo={this.playVideo} style={this.state.searchResultsStyle} />
           </div> 
-         <div>
+          <div>
             <webview id="webview" src={this.state.webviewSrc} style={this.state.webviewStyle}></webview> 
           </div>
         </div>
