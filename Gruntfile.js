a2d6e99281b5e293c9d175d820dfb2e234655e1e
Frank Hale <frankhale@gmail.com>
2016-12-27 11:55:55 -0500

    beautify files

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,37 +1,38 @@
-module.exports = function(grunt) {
-    grunt.initConfig({
-      copy: {
-        main: {
-          files: [
-            { src: ["index.html"], dest: "build/" },
-          ]
-        }
-      },
-      ts: {
-        default: {
-          src: ["src/*.ts"],
-          outDir: "build",
-          options: {
-            sourceMap: true,
-            target: "es6",
-            module: "commonjs"
-          }
-        }
-      },
-      tslint: {
-        files: {
-          src: ["src/*.ts*", "src/react-components/*.ts*"]
-        },
+module.exports = function (grunt) {
+  grunt.initConfig({
+    copy: {
+      main: {
+        files: [{
+          src: ["index.html"],
+          dest: "build/"
+        }, ]
+      }
+    },
+    ts: {
+      default: {
+        src: ["src/*.ts"],
+        outDir: "build",
         options: {
-          force: false,
-          fix: false
+          sourceMap: true,
+          target: "es6",
+          module: "commonjs"
         }
       }
-    });
+    },
+    tslint: {
+      files: {
+        src: ["src/*.ts*", "src/react-components/*.ts*"]
+      },
+      options: {
+        force: false,
+        fix: false
+      }
+    }
+  });
 
-    grunt.loadNpmTasks("grunt-ts");
-    grunt.loadNpmTasks("grunt-contrib-copy");
-    grunt.loadNpmTasks("grunt-tslint");
+  grunt.loadNpmTasks("grunt-ts");
+  grunt.loadNpmTasks("grunt-contrib-copy");
+  grunt.loadNpmTasks("grunt-tslint");
 
-    grunt.registerTask("default", ["ts", "copy", "tslint"]);
+  grunt.registerTask("default", ["ts", "copy", "tslint"]);
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,53 @@
 var path = require("path");
 
 module.exports = {
-    entry: [
-      "command-input-ui.tsx",
-      "dropdown-ui.tsx",
-      "infrastructure.ts",
-      "toby-ui.tsx",
-      "version-ui.tsx",
-      "video-list-grid-ui.tsx",
-      "video-list-ui.tsx",
-      "youtube-ui.tsx"
+  entry: [
+    "command-input-ui.tsx",
+    "dropdown-ui.tsx",
+    "infrastructure.ts",
+    "toby-ui.tsx",
+    "version-ui.tsx",
+    "video-list-grid-ui.tsx",
+    "video-list-ui.tsx",
+    "youtube-ui.tsx"
+  ],
+  output: {
+    filename: "app.js",
+    path: __dirname + "/public/scripts"
+  },
+  devtool: "source-map",
+  resolve: {
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: ["", ".webpack.js", ".web.js", ".ts", ".tsx", ".js"],
+    root: [
+      path.resolve("./src/react-components")
+    ]
+  },
+  module: {
+    loaders: [
+      // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+      {
+        test: /\.tsx?$/,
+        loader: "awesome-typescript-loader"
+      }
     ],
-    output: {
-      filename: "app.js",
-      path: __dirname + "/public/scripts"
-    },
-    devtool: "source-map",
-    resolve: {
-      // Add '.ts' and '.tsx' as resolvable extensions.
-      extensions: ["", ".webpack.js", ".web.js", ".ts", ".tsx", ".js"],
-      root: [
-        path.resolve("./src/react-components")
-      ]
-    },
-    module: {
-      loaders: [
-          // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
-          { test: /\.tsx?$/, loader: "awesome-typescript-loader" }
-      ],
-      preLoaders: [
-          // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-          { test: /\.js$/, loader: "source-map-loader" }
-      ]
-    },
-    // When importing a module whose path matches one of the following, just
-    // assume a corresponding global variable exists and use that instead.
-    // This is important because it allows us to avoid bundling all of our
-    // dependencies, which allows browsers to cache those libraries between builds.
-    externals: {
-      "react": "React",
-      "react-dom": "ReactDOM",
-      "lodash": "_",
-      "socket.io-client": "io",
-      "jquery": "$"
-    },
+    preLoaders: [
+      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+      {
+        test: /\.js$/,
+        loader: "source-map-loader"
+      }
+    ]
+  },
+  // When importing a module whose path matches one of the following, just
+  // assume a corresponding global variable exists and use that instead.
+  // This is important because it allows us to avoid bundling all of our
+  // dependencies, which allows browsers to cache those libraries between builds.
+  externals: {
+    "react": "React",
+    "react-dom": "ReactDOM",
+    "lodash": "_",
+    "socket.io-client": "io",
+    "jquery": "$"
+  },
 };
